5b00eff02177d127e30e2cdc8e8454f85e3e6b3d
dilireba12 <2845414681@qq.con>
2022-05-19 15:03:49 +0800

    尚未完成的代码

---
+++ assets/js/login.js
@@ -0,0 +1,63 @@
+$(function () {
+    // 点击切换效果
+    $("#link_reg").click(() => {
+        $(".login-box").hide();
+        $(".reg-box").show();
+    });
+    $("#link_login").click(() => {
+        $(".login-box").show();
+        $(".reg-box").hide();
+    });
+    // 获取form
+    const form = layui.form;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/ , '密码必须6到12位，且不能出现空格'],
+        // 定义确认密码规则
+        repwd:(val) =>{
+            const pwd = $('.reg-box [name=password]').val();
+            if(val !== pwd) return "两次密码不一致!";
+        },
+    })
+   // 获取 layui 弹窗
+const layer = layui.layer;
+// 设置请求根路径
+const baseUrl = "http://www.liulongbin.top:3007";
+    // 监听注册提交表单，发送注册请求
+    $("#form_reg").on("submit",function(e){
+        // （e）阻止form 默认提交行为
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url:baseUrl + "/api/reguser",
+            data:{
+                username:$("#form_reg [name=username]").val(),
+                password:$("#form_reg [name=password]").val(),
+            },
+            success:(res) =>{
+                if(res.status !== 0) return layer.msg("注册失败！");
+                layer.msg("注册成功!");
+                $("#link_login").click();
+            }
+
+        })
+    })
+    // 监听登录提交表单，发送登录请求
+    $("#form_login").on("submit",function(e){
+        // （e）阻止form 默认提交行为
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url:baseUrl + "/api/login",
+            data:
+               $(this).serialize(),
+            
+            success:(res) =>{
+                if(res.status !== 0) return layer.msg("登录失败！");
+                console.log(res);
+                layer.msg("登录成功!");
+               
+            }
+
+        })
+    })
+})
